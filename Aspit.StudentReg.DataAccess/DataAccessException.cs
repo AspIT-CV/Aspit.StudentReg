3b5172db56ef352d7c95c594c2a89eb0a9d330f2
Mads Mikkel Rasmussen <mara@aspit.dk>
2018-11-01 13:35:47 +0100

    Added ctor and TryConnect method in RepositoryBase.

---
+++ Aspit.StudentReg.DataAccess/DataAccessException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Aspit.StudentReg.DataAccess
+{
+    [Serializable]
+    internal class DataAccessException: Exception
+    {
+        public DataAccessException()
+        {
+        }
+
+        public DataAccessException(string message) : base(message)
+        {
+        }
+
+        public DataAccessException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected DataAccessException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
--- Aspit.StudentReg.DataAccess/RepositoryBase.cs
+++ Aspit.StudentReg.DataAccess/RepositoryBase.cs
@@ -3,10 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Aspit.StudentReg.DataAccess
 {
-    public class RepositoryBase
+    /// <summary>
+    /// Base class for repositories.
+    /// </summary>
+    public abstract class RepositoryBase
     {
+        /// <summary>
+        /// The connection string, used to connect to the persistence server.
+        /// </summary>
+        protected readonly string connectionString;
+
+        /// <summary>
+        /// Base constructor. Call this from deriving constructors.
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
+        public RepositoryBase(string connectionString)
+        {
+            if(String.IsNullOrEmpty(connectionString))
+            {
+                throw new DataAccessException("Invalid connection string - was null or empty");
+            }
+            try
+            {
+                TryConnect(connectionString);
+            }
+            catch(DataAccessException)
+            {
+                throw;
+            }
+            this.connectionString = connectionString;
+        }
+
+        /// <summary>
+        /// Attempts to create connection to the server, using the provided connection string.
+        /// </summary>
+        /// <param name="connectionString"></param>
+        internal static void TryConnect(string connectionString)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                connection.Close();
+            }
+            catch(Exception e)
+            {
+                throw new DataAccessException("Connection attempt failed.", e);
+            }
+        }
     }
 }

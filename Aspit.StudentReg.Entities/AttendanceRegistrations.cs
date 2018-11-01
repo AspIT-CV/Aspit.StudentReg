5c31a08e09a095c2c8a3f94248efa4eb0c072063
mort358d <mort358d@CV-BB-5860>
2018-11-01 14:03:53 +0100

    Created the attendance registration class

---
+++ Aspit.StudentReg.Entities/AttendanceRegistrations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aspit.StudentReg.Entities
+{
+    /// <summary>
+    /// Represents an attendance registration
+    /// </summary>
+    class AttendanceRegistrations
+    {
+    }
+}

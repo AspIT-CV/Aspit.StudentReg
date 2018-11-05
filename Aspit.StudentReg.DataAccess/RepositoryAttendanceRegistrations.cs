b6676a09a270f7f12b27b2fb2c1b3272be369744
mort358d <mort358d@CV-BB-5860>
2018-11-05 09:13:04 +0100

    Created RepositoryAttendanceRegistrations' empty methods

--- Aspit.StudentReg.DataAccess/RepositoryAttendanceRegistrations.cs
+++ Aspit.StudentReg.DataAccess/RepositoryAttendanceRegistrations.cs
@@ -14,12 +14,40 @@ namespace Aspit.StudentReg.DataAccess
     class RepositoryAttendanceRegistrations : RepositoryBase
     {
         /// <summary>
-        /// Intializes a new Repository for AttendanceRegistration using the given connection string.
+        /// Intializes a new Repository for <see cref="AttendanceRegistration"/> using the given connection string.
         /// </summary>
         /// <param name="connectionString">the connection string used to connect to the database</param>
         public RepositoryAttendanceRegistrations(string connectionString) : base(connectionString)
         {
 
+        }
+
+        /// <summary>
+        /// Updates the given <see cref="AttendanceRegistration"/> in the database
+        /// </summary>
+        /// <param name="attendanceRegistration">The <see cref="AttendanceRegistration"/> to update</param>
+        public void Update(AttendanceRegistration attendanceRegistration)
+        {
+            //TODO create the update method
+        }
+
+        /// <summary>
+        /// Creates the given <see cref="AttendanceRegistration"/> in the database
+        /// </summary>
+        /// <param name="attendanceRegistration">The <see cref="AttendanceRegistration"/> to create</param>
+        public void Create(AttendanceRegistration attendanceRegistration)
+        {
+            //TODO create the create method
+        }
+
+        /// <summary>
+        /// Gets all <see cref="AttendanceRegistration"/>s from the database
+        /// </summary>
+        /// <returns>a <see cref="IEnumerator<AttendanceRegistration>"/> containing all the <see cref="AttendanceRegistration"/>s</returns>
+        public IEnumerator<AttendanceRegistration> Get()
+        {
+            //TODO create the get method
+            yield break;
         }
     }
 }

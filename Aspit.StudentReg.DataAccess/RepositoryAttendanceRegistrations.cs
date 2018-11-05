33f1a0a4897c79f0d74be45cc7bdbc79e25d9b21
mort358d <mort358d@CV-BB-5860>
2018-11-05 09:06:21 +0100

    Added entity reference to RepositoryAttendanceRegistrations

--- Aspit.StudentReg.DataAccess/RepositoryAttendanceRegistrations.cs
+++ Aspit.StudentReg.DataAccess/RepositoryAttendanceRegistrations.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Aspit.StudentReg.Entities;
+
 
 namespace Aspit.StudentReg.DataAccess
 {

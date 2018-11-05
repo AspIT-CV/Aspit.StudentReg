fba8d7670cbfddcc919fa21facceca8a5a30a042
mort358d <mort358d@CV-BB-5860>
2018-11-05 13:46:31 +0100

    Added AttendanceRegistrationsRepositoryTests class

---
+++ Aspit.StudentReg.DataAccessTests/AttendanceRegistrationsRepositoryTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aspit.StudentReg.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aspit.StudentReg.DataAccess.Tests
+{
+    [TestClass()]
+    public class AttendanceRegistrationsRepositoryTests
+    {
+
+    }
+}

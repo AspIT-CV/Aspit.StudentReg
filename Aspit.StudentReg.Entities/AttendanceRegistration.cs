87705c1ce4efa5566f0e8ce14f5fdbf4feb645b2
mort358d <mort358d@CV-BB-5860>
2018-11-05 10:22:46 +0100

    Removed userForeignKey field and property

--- Aspit.StudentReg.Entities/AttendanceRegistration.cs
+++ Aspit.StudentReg.Entities/AttendanceRegistration.cs
@@ -17,11 +17,6 @@ namespace Aspit.StudentReg.Entities
         private int id;
 
         /// <summary>
-        /// The id of the User this AttendanceRegistration is for
-        /// </summary>
-        private int userForeignKey;
-
-        /// <summary>
         /// The time the user signed in
         /// </summary>
         private DateTime meetingTime;
@@ -35,14 +30,12 @@ namespace Aspit.StudentReg.Entities
         /// Intializes a new AttendanceRegistration using the given parameters.
         /// </summary>
         /// <param name="id">the registration id</param>
-        /// <param name="userForeignKey">the id of user this attendance is for</param>
         /// <param name="meetingTime">the time the user signed in</param>
         /// <param name="leaveTime">the time the user signed out</param>
         /// <param name="date">the day this attendance is about</param>
-        public AttendanceRegistration(int id, int userForeignKey, DateTime meetingTime, DateTime leaveTime)
+        public AttendanceRegistration(int id, DateTime meetingTime, DateTime leaveTime)
         {
             Id = id;
-            UserForeignKey = userForeignKey;
             MeetingTime = meetingTime;
             LeaveTime = leaveTime;
         }
@@ -61,23 +54,6 @@ namespace Aspit.StudentReg.Entities
                     throw new ArgumentOutOfRangeException("Id cannot be less than 0.");
                 }
                 id = value;
-            }
-        }
-
-        public int UserForeignKey
-        {
-            get
-            {
-                return userForeignKey;
-            }
-
-            set
-            {
-                if(value < 0)
-                {
-                    throw new ArgumentOutOfRangeException("UserForeignKey cannot be less than 0");
-                }
-                userForeignKey = value;
             }
         }
 

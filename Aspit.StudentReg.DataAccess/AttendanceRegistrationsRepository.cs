f5936c449d3c358385611725b5a84e57d9ce445c
mort358d <mort358d@CV-BB-5860>
2018-11-05 11:55:52 +0100

    Added attendanceRegistrationRepository GetFromId

--- Aspit.StudentReg.DataAccess/AttendanceRegistrationsRepository.cs
+++ Aspit.StudentReg.DataAccess/AttendanceRegistrationsRepository.cs
@@ -66,22 +66,40 @@ namespace Aspit.StudentReg.DataAccess
 
 
         /// <summary>
-        /// Gets the given <see cref="User"/>'s current <see cref="AttendanceRegistration"/> from the database
+        /// Gets the AttendanceRegistration with the given id
         /// </summary>
-        /// <param name="student">the <see cref="User"/> to get the <see cref="AttendanceRegistration"/> from</param>
-        /// <returns>the <see cref="User"/>'s current <see cref="AttendanceRegistration"/></returns>
-        public AttendanceRegistration GetStudentsCurrentRegistration(User student)
+        /// <param name="id">the id</param>
+        /// <returns>a AttendanceRegistration from the database</returns>
+        public AttendanceRegistration GetFromId(int id)
         {
-            //TODO create the GetFromId method
-            throw new NotImplementedException();
+            SqlCommand getCommand = new SqlCommand("SELECT * FROM AttandanceRegistrations WHERE Id=@Id");
+            getCommand.Parameters.AddWithValue("@Id",id);
+            DataSet getOutput = Execute(getCommand);
+
+            if(getOutput.Tables.Count < 0)
+            {
+                throw new DataAccessException("Failed to get any tables from database");
+            }
+            else
+            {
+                List<AttendanceRegistration> registrations = ConvertDateRowsIntoAttendanceRegistrations(getOutput.Tables[0].Rows);
+                if(registrations.Count != 1)
+                {
+                    throw new DataAccessException("Failed to get an AttendanceRegistration with the given id");
+                }
+                else
+                {
+                    return registrations[0];
+                }
+            }
         }
 
         /// <summary>
-        /// Gets the given <see cref="User"/>'s <see cref="AttendanceRegistration"/>s from the database
+        /// Gets the given <see cref="Student"/>'s <see cref="AttendanceRegistration"/>s from the database
         /// </summary>
-        /// <param name="student">the <see cref="User"/> to get the <see cref="AttendanceRegistration"/>s from</param>
+        /// <param name="student">the <see cref="Student"/> to get the <see cref="AttendanceRegistration"/>s from</param>
         /// <returns>a lsit containing all the <see cref="AttendanceRegistration"/>s for the student</returns>
-        public List<AttendanceRegistration> GetUsersRegistrations(User student)
+        public List<AttendanceRegistration> GetUsersRegistrations(Student student)
         {
             //TODO create the GetUsersRegistrations method
             throw new NotImplementedException();
@@ -108,22 +126,20 @@ namespace Aspit.StudentReg.DataAccess
                 List<AttendanceRegistration> returnList = new List<AttendanceRegistration>();
                 foreach(DataRow row in dataRows)
                 {
-                    int id;
-                    DateTime? meetingTime;
-                    DateTime? leavingTime;
+                    AttendanceRegistration registration = default;
 
                     try
                     {
-                        id = row.Field<int>("MeetingTime");
-                        meetingTime = row.Field<DateTime>("MeetingTime");
-                        leavingTime = row.Field<DateTime>("MeetingTime");
+                        registration.Id = row.Field<int>("MeetingTime");
+                        registration.MeetingTime = row.Field<DateTime>("MeetingTime");
+                        registration.LeavingTime = row.Field<DateTime>("MeetingTime");
                     }
                     catch(InvalidCastException)
                     {
                         throw new DataAccessException("Failed to convert table row into the needed AttendanceRegistration properties");
                     }
 
-                    returnList.Add(new AttendanceRegistration(id, meetingTime, leavingTime));
+                    returnList.Add(registration);
                 }
 
                 return returnList;

b4f8859db8dab7bf91b20e06c38fb7c218d07f3c
mort358d <mort358d@CV-BB-5860>
2018-11-05 09:05:03 +0100

    Created the RepositoryAttendanceRegistrations class.

---
+++ Aspit.StudentReg.DataAccess/RepositoryAttendanceRegistrations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aspit.StudentReg.DataAccess
+{
+    /// <summary>
+    /// The repository for AttendanceRegistrations
+    /// </summary>
+    class RepositoryAttendanceRegistrations : RepositoryBase
+    {
+        /// <summary>
+        /// Intializes a new Repository for AttendanceRegistration using the given connection string.
+        /// </summary>
+        /// <param name="connectionString">the connection string used to connect to the database</param>
+        public RepositoryAttendanceRegistrations(string connectionString) : base(connectionString)
+        {
+
+        }
+    }
+}

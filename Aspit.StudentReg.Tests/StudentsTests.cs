9944585b6ce4c78581ad14ef820aa5824774c78d
Magnusbou <Magnusb@ethyria.dk>
2018-11-05 11:19:43 +0100

    Added Aspit.StudentReg.Entities; to unittest

---
+++ Aspit.StudentReg.Tests/StudentsTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aspit.StudentReg.Entities;
+
+namespace Aspit.StudentReg.Tests
+{
+    [TestClass]
+    public class StudentsTests
+    {
+        [TestMethod]
+        public void Initialization()
+        {
+           
+        }
+    }
+}

8f7cd3d20d66e75057248a1aaf764f33bb040127
Magnusbou <Magnusb@ethyria.dk>
2018-11-07 08:17:04 +0100

    Improved user unitTest

--- Aspit.StudentReg.Tests/StudentsTests.cs
+++ Aspit.StudentReg.Tests/StudentsTests.cs
@@ -12,28 +12,45 @@ namespace Aspit.StudentReg.Tests
     [TestClass]
     public class StudentsTests
     {
+        Student student;
+        int id = 1;
+        string name = "per Bosen";
+        string expectedName = "Per Bosen";
+        string uniLogin = "perx234k";
+
+        public StudentsTests()
+        {
+            student = new Student(id, name, uniLogin);
+        }
+
+
         [TestMethod]
         public void Initialization()
         {
-            //Arrange
-            Student s;
-
-            int id = 1;
-
-            string name = "per Bosen";
-            string expectedName = "Per Bosen";
-
-            string uniLogin = "perx234k";
-
-            //Act
-
-            s = new Student(id, name, uniLogin);
-
-            //Assert
-
-            Assert.AreEqual(id, s.Id);
-            Assert.AreEqual(expectedName, s.Name);
-            Assert.AreEqual(uniLogin, s.UniLogin);
+            Assert.AreNotEqual(null, student);
         }
+
+        [TestMethod]
+        public void ValueTest()
+        {
+            Assert.AreEqual(id, student.Id);
+            Assert.AreEqual(expectedName, student.Name);
+            Assert.AreEqual(uniLogin, student.UniLogin);
+        }
+
+        [TestMethod]
+        public void AttendanceRegistrationsAssignment()
+        {
+            AttendanceRegistration registration = new AttendanceRegistration
+            {
+                Id = 1,
+                LeavingTime = new DateTime(2018, 6, 11, 23, 59, 59),
+                MeetingTime = new DateTime(2018, 6, 11, 9, 0, 0)
+            };
+
+            student.AttendanceRegistrations = registration;
+            Assert.AreEqual(registration, student.AttendanceRegistrations);
+        }
+
     }
 }
